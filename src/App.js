d7a0e2476702f364b305f34b651ce4ee0fac136d
Jrgithb <40182730@hruc.ac.uk>
2023-12-18 15:23:43 +0000

    Reservations/events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Reservation from './Views/Reservation/Reservation';
+import Events from './Views/Reservation/Events/Events';
 
 function App() {
   return (
     <div className="App">
       <Reservation/>
+      <Events/>
     </div>
   );
 }
