89da8256f8c6ef332655b905c67dc2afd5b5c9f3
Jrgithb <40182730@hruc.ac.uk>
2023-12-18 13:32:45 +0000

    reservation

---
+++ src/Views/Reservation/Reservation.js
@@ -0,0 +1,23 @@
+import "./Reservation.css"
+ 
+const Reservation = () => {
+    return (
+      <div className="reservation">
+        <div className="div">
+         
+          <div className="overlap-group">
+            <div className="div-block">
+              <div className="frame">
+                <div className="text-wrapper"></div>
+                <div className="booking-btn">BOOK A TABLE</div>
+               
+              </div>
+            </div>
+            <div className="text-wrapper-2">Make a Reservation</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+ 
+  export default Reservation;
