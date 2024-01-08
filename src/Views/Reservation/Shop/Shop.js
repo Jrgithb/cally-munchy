2d157f6468f7fa62ccc3c5e8d2c28a3da7e0a30e
Jrgithb <40182730@hruc.ac.uk>
2024-01-08 16:40:00 +0000

    reservations/events

--- src/Views/Reservation/Shop/Shop.js
+++ src/Views/Reservation/Shop/Shop.js
@@ -9,6 +9,7 @@ const Shop = () => {
                 and packed it into one mighty bottle for the customers pleasure.
             </p>
             <div className="find-out-more-btn">Find out more</div>
+            <div className="shop-header-img"><img src="https://s3-alpha-sig.figma.com/img/2e89/4983/55f5f01af0b63cb6adfe103239795672?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YDC~6UQEreL5U41scBOkZWAbSBgk7TsKQUQAgr8is0M1ZoG5qV01ey0nZaizUsDT0q2GWh6FurYo~53cihRSTjr3820M23fykAJVZ9sn7zdV0cvmx0zsRI3rqs-6C7UHNU7T7dXnbndfbz8dRjs77HNtDLHF79WLSTo7065~oI~SrPZL~XGWemNLTtFyKtHO~LmLx4Ae1kGo5E9xy6HZXjqD-HSMZ2BNDaGVjkvN0iZbq5ultJ1tZqr53hM-b-priBhEANqUWR87N2fODB3Z3mg70qgDcrsYtrs0HmPZdjrSJUDxiRb4hx-RhZSuiVBV9Oj0K6PHBqE0Rh-Scsw4Jw__"/></div>
 
         </div>
     )
