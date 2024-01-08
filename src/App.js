64892bd10114b68ec5116d4c71c93c629226e396
Jrgithb <40182730@hruc.ac.uk>
2024-01-08 13:28:02 +0000

    reservations/events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Reservation from './Views/Reservation/Reservation';
 import Events from './Views/Reservation/Events/Events';
 import Calender from './Views/Reservation/Calender/Calender';
 import Drinks from './Views/Reservation/Drinks/Drinks';
@@ -10,10 +9,10 @@ import Shop from './Views/Reservation/Shop/Shop';
 function App() {
   return (
     <div className="App">
-      <Reservation/>
       <Events/>
       <Calender/>
       <Drinks/>
+      <Shop/>
     </div>
   );
 }
--- src/Views/Reservation/Shop/Shop.js
+++ src/Views/Reservation/Shop/Shop.js
@@ -2,8 +2,14 @@ import "./Shop.css"
 
 const Shop = () => {
     return(
-        <div className="label">
-            <div></div>
+        <div className="shop-header">
+            <div className="shop-section">SHOP OUR PRODUCTS</div>
+            <p className="our-chilli-sauce">Our Chilli sauce called Shi-Tuan is a cultural burst of sweet-heat. 
+                We've captured the flavour of some of the best chilli oils from across the globe 
+                and packed it into one mighty bottle for the customers pleasure.
+            </p>
+            <div className="find-out-more-btn">Find out more</div>
+
         </div>
     )
 };
