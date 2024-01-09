8c4cc9757de254ba77819da31b1166ebdf25f5f8
Jrgithb <40182730@hruc.ac.uk>
2024-01-09 14:03:50 +0000

    reservation/events

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Events from './Views/Reservation/Events/Events';
 import Calender from './Views/Reservation/Calender/Calender';
-import Drinks from './Views/Reservation/Drinks/Drinks';
 import Shop from './Views/Reservation/Shop/Shop';
-import Eform from './Views/Reservation/Eform/Eform';
+
 
 
 function App() {
@@ -12,9 +11,7 @@ function App() {
     <div className="App">
       <Events/>
       <Calender/>
-      <Drinks/>
       <Shop/> 
-      <Eform/>
     </div>
   );
 }
